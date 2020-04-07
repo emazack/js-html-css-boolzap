c24d5a00a14cb0b5727774b6804e6ba009d2fe21
Emanuele <em.zaccaria@gmail.com>
2020-04-07 16:49:04 +0200

    JS invio messaggio

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
-  
+  // salvataggio e creazione variabili
+  var tastoInvio = $(".tasto-invio");
+
+  // al click c'è l'invio del messaggio
+  tastoInvio.click(
+    function(){
+      var testoInput = $(".barra-scrittura input").val();
+      $(".finestra-messaggi").append("<div class='mex-inviato'><p>" + testoInput + "</p></div>");
+      $(".barra-scrittura input").val("Scrivi un messaggio");
+    }
+  )
+
+  // function () {
+  //    var input = $('input').val();
+  //    // console.log(input);
+  //    contenitore.append("<li>" + input + "</li>");
+  //    $('input').val("");
+  //  }
+
+
+
 });
