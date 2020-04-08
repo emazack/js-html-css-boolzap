7538b344a4e6e6f668cb4e54ce9c3f4a37dc0ff7
Emanuele <em.zaccaria@gmail.com>
2020-04-08 16:13:22 +0200

    JS Messaggio automatico dopo 1 secondo

--- main.js
+++ main.js
@@ -5,9 +5,16 @@ $(document).ready(function(){
   // al click c'è l'invio del messaggio
   tastoInvio.click(
     function(){
+      // prendo il valore presente nella barra di scrittura e lo salvo
       var testoInput = $(".barra-scrittura input").val();
+      // inserisco nella finestra messaggi ciò che è scritto nella barra di scrittura
       $(".finestra-messaggi").append("<div class='mex-inviato'><p>" + testoInput + "</p></div>");
+      // azzero il valore della barra di scrittura
       $(".barra-scrittura input").val("Scrivi un messaggio");
+      // invio di messaggio automatico
+      setTimeout(function(){
+        $(".finestra-messaggi").append("<div class='mex-ricevuto'><p> Sae </p></div>");
+      }, 1000);
     }
   )
 });
